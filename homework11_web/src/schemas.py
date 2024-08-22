89d69bd564b8288792819b6a20e9266841934dda
stanislavsynyasvskyy <153124369+sasykk@users.noreply.github.com>
2024-08-22 21:25:24 +0300

    Add files via upload

---
+++ homework11_web/src/schemas.py
@@ -0,0 +1,25 @@
+from datetime import date
+from typing import Optional
+from pydantic import BaseModel
+
+class ContactBase(BaseModel):
+    first_name: str
+    last_name: str
+    email: str
+    phone_number: str
+    birthday: date
+    additional_info: Optional[str] = None
+
+class ContactCreate(ContactBase):
+    pass
+
+class ContactUpdate(ContactBase):
+    pass
+
+class ContactInDBBase(ContactBase):
+    id: int
+    class Config:
+        orm_mode = True
+
+class Contact(ContactInDBBase):
+    pass
